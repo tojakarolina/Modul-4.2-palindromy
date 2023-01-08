0b8626a16bd9e68346ac2b6d031db058f312a572
tojakarolina <karolina.przytula1997@gmail.com>
2023-01-08 15:24:52 +0100

    Add compare between two words in modul-4.2-zad-palindromy.py

--- modul-4.2-zad-palindromy.py
+++ modul-4.2-zad-palindromy.py
@@ -2,9 +2,11 @@ def if_palindrom (word):
     word_2 = ""
     for letter in range( len(word) -1, -1, -1):
             word_2 += word[letter]
+    if word_2 == word:
+            return True
+    else:
+            return False
 
-    return word_2
-
-palindrom = if_palindrom("kocyk")
+palindrom = if_palindrom("kajak")
 print(palindrom)
     
